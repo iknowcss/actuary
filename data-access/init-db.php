4c66e11548cf30e1bd6a8ac2589c332e5c2096e4
Cody Jenkins <iknowcss@gmail.com>
2014-07-27 16:07:38 -0700

    Generalize database component to work in dev and in prod

--- data-access/init-db.php
+++ data-access/init-db.php
@@ -1,9 +1,34 @@
 <?php
 
   function getDb() {
-    $mongoClient = new MongoClient();
-    $db = $mongoClient->actuary;
-    return $db;
+    $dbUser = getenv('MONGO_DB_USER');
+    $dbPass = getenv('MONGO_DB_PASS');
+    $dbHost = getenv('MONGO_DB_HOST');
+    $dbPort = getenv('MONGO_DB_PORT');
+    $dbName = getenv('MONGO_DB_NAME');
+
+    if (!$dbHost) {
+      $dbHost = 'localhost';
+    }
+
+    if ($dbUser && $dbPass) {
+      $dbCredentials = "$dbUser:$dbPass@";
+    } else {
+      $dbCredentials = '';
+    }
+
+    if ($dbPort) {
+      $dbPort = ":$dbPort";
+    }
+
+    if (!$dbName) {
+      $dbName = 'actuary';
+    }
+
+    $dbConnectionString = "mongodb://${dbCredentials}${dbHost}${dbPort}";
+    $m = new MongoClient($dbConnectionString, array("db" => $dbName));
+
+    return $m->$dbName;
   }
 
 ?>
